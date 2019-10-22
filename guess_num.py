ae35d4f2c2d0046b711c960753b45bcfc15108d4
李明叡 <leeray1208@limingruide-MacBook-Pro.local>
2019-10-21 23:17:31 -0700

    add count

--- guess_num.py
+++ guess_num.py
@@ -6,14 +6,19 @@
 import random # 載入模組 才能產生隨機數
 
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1	# count = count + 1
 	num = input('Please guess a number within 1 to 100: ')
 	num = int(num)	# casting is important
 	if num == r:
 		print("Finally correct!")
+		print("This is your %d times" %count)
 		break
 	elif num > r:
 		print("Guess is larger than answer")
 	else:
 		print("Guess is lower than answer")
-	
+	print("This is your %d times" %count)	# 寫在這 避免重複寫三次 避免重複性太高的問題 
+											# 但是這樣就不會算到猜中這次
+
